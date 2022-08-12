9bc9a45655e7e326118e7e0f139351baacedbbcc
rcg7 <racaga85@gmail.com>
2022-08-12 06:40:12 +0200

    Add index.php usuarios.php package.json package-lock.json nosotros.php login.php gulpfile.js entrada.php contacto.php composer.json composer.lock cerrar-sesion.php blog.php base.php anuncios.php anuncio.php

---
+++ anuncios.php
@@ -0,0 +1,23 @@
+<?php 
+    require 'includes/app.php';
+    incluirTemplate('header'); 
+?>
+
+    <main class="contenedor seccion">
+
+        <h2>Casas y Depas en Venta</h2>
+
+        <?php
+
+            $limite = 9;
+            include 'includes/templates/anuncios.php';
+        ?>
+        
+        </div> <!--.contenedor-anuncios-->
+    </main>
+
+<?php 
+        incluirTemplate('footer');
+?>
+
+  
